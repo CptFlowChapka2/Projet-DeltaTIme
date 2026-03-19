1ac974286bf5bce475449e0deadd1962dc9d6816
CptFlowChapka2 <eterrien5@myges.fr>
2026-03-19 03:54:07 +0100

    dazopjdiz

--- Assets/NewArchitecture/Feedbacks/BeatMarkerScript.cs
+++ Assets/NewArchitecture/Feedbacks/BeatMarkerScript.cs
@@ -5,18 +5,25 @@ public class BeatMarkerScript : MonoBehaviour
 {
    public Transform[] allPoints = new Transform[] { };
 
-   private int currentIndex = 0;
+   public int currentIndex = 0;
    private float currentPercent = 0;
-   public 
+   public float percentByFixedUpdate=0.032f;
 
    private void FixedUpdate()
    {
+      if (currentPercent >= 1)
+      {
+         currentPercent = 0;
+         currentIndex++;
+      }
       if (currentIndex == 3)
       {
          currentIndex = 0;
          transform.position = allPoints[0].position;
       }
+      
       transform.position = Vector3.Lerp(allPoints[currentIndex].position, allPoints[currentIndex + 1].position, currentPercent);
-      currentPercent
+      currentPercent += percentByFixedUpdate;
+      
    }
 }
