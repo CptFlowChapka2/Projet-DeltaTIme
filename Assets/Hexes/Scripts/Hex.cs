bcca2e940a1b4ede992e1dbb91382bac2808cee8
COLIN-MJM <matthieu.j.m.colin@gmail.com>
2026-03-31 14:01:47 +0200

    Refonte de la grille (tout est mieux normalement) +début feedback hover

--- Assets/Hexes/Scripts/Hex.cs
+++ Assets/Hexes/Scripts/Hex.cs
@@ -3,20 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+public enum HexState
+{
+    None,
+    Idle,
+    Hovered
+}
+
 public abstract class Hex : MonoBehaviour
 {
     [SerializeField] protected Vector2Int relativeCoords;
     public List<Grabbable> grabbablesOnThisHex = new List<Grabbable>();
     public Vector3 boundsCenter;
+    public HexState state = HexState.Idle;
+    [SerializeField] private Material idleMaterial;
+    [SerializeField] private Material hoveredMaterial;
+    private MeshRenderer meshRenderer;
 
     private void Awake()
     {
-        boundsCenter = GetComponent<MeshRenderer>().bounds.center;
+        meshRenderer = GetComponent<MeshRenderer>();
+        boundsCenter = meshRenderer.bounds.center;
+    }
+    
+    private void Update()
+    {
+        OnUpdate();
+    }
+
+    public virtual void OnUpdate()
+    {
+        switch (state)
+        {
+            case HexState.Idle:
+                ChangeMaterial(idleMaterial);
+                break;
+            case HexState.Hovered:
+                ChangeMaterial(hoveredMaterial);
+                break;
+            default:
+                ChangeMaterial(idleMaterial);
+                break;
+        }
     }
 
     public virtual void Tick()
     {
         grabbablesOnThisHex.First().Tick();
+    }
+
+    public void ChangeMaterial(Material newMaterial)
+    {
+        meshRenderer.material = newMaterial;
     }
 
     public virtual void AddGrabbable(Grabbable grabbable)
@@ -46,6 +84,4 @@ public abstract class Hex : MonoBehaviour
         allNextGrabbables.RemoveRange(0, grabbablesOnThisHex.IndexOf(grabbable) + 1);
         return allNextGrabbables;
     }
-    
-    
 }
