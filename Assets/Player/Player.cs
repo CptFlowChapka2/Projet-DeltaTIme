b36d56891a7f01ab248b34c4c7e2fe8cc516548f
COLIN-MJM <matthieu.j.m.colin@gmail.com>
2026-04-04 20:10:33 +0200

    revue des contrôles, rotation maintenant sur RigidBody

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -16,14 +16,14 @@ public class Player : Grabbable
     private InputAction spinAction;
     private InputAction extendRetractAction;
     private InputAction grabReleaseAction;
-    [SerializeField] private GameObject visualArm;
-   
+    public Rigidbody rb;
     
-
-    public Vector3 arm;
+    public Vector3 armVector;
     
+    [SerializeField] private GameObject physicalArm;
     public MeshRenderer popupRenderer;
     public Grabber grabber;
+    [SerializeField] private Transform armOrigin;
 
     protected override void Start()
     {
@@ -31,8 +31,9 @@ public class Player : Grabbable
         grabber.player = this;
         popupRenderer.gameObject.GetComponent<PopupMover>().playerToFollow = this;
         transform.position += new Vector3(0, 0.5f, 0);
-        arm = new Vector3(1, 0, 0);
+        armVector = new Vector3(0, 0, 2.5f);
         playerInput = GetComponent<PlayerInput>(); 
+        rb = GetComponent<Rigidbody>();
         spinAction = playerInput.actions["Spin"];
         extendRetractAction = playerInput.actions["Extend"];
         grabReleaseAction = playerInput.actions["Grab"];
@@ -44,30 +45,36 @@ public class Player : Grabbable
         {
             ActualVariant = 0;
             GrabRelease();
-            Vector3 tempArm = arm;
-            ExtendRetract1Axis(tempArm,out  tempArm);
-            Spin1Axis(out float spinValue,tempArm,out tempArm);
-        
-            RaycastHit[] hits = Physics.BoxCastAll(transform.position + tempArm / 2,
-                new Vector3(10f, tempArm.magnitude/2,0.2F), Vector3.up,
-                visualArm.transform.rotation * Quaternion.AngleAxis(spinValue, Vector3.right),0,-1,QueryTriggerInteraction.Collide);
-        
-            var hitlist = hits.ToList();
-            hitlist.RemoveAll(x=>x.collider.gameObject==visualArm ||
-                                 x.collider.gameObject==grabber.gameObject);
-            //todo revoir le blocker car du coup ça déconne quand on grab l'autre joueur
-            //Debug.Log(hitlist.Count);
-            //if(hitlist.Any(x=>x.collider.gameObject.CompareTag("ArmBlocker")))return;
-        
-            arm = tempArm;
-            grabber.transform.position = transform.position + arm;
-            visualArm.transform.rotation*=Quaternion.AngleAxis(spinValue, Vector3.right);
-            UpadateVisualArm();
+            
+            Vector3 tempArm = armVector;
+            ExtendRetract1Axis(out tempArm);
+            armVector = tempArm;
+            grabber.transform.position = transform.position + armVector;
+            UpdateVisualArm();
         }
         else
         {
             ActualVariant = 1;
         }
+    }
+
+    private void FixedUpdate()
+    {
+        Spin1Axis();
+    }
+
+    private void OriginalBlocker(Vector3 tempArm, float spinValue)
+    {
+        RaycastHit[] hits = Physics.BoxCastAll(transform.position + tempArm / 2,
+            new Vector3(10f, tempArm.magnitude/2,0.2F), Vector3.up,
+            physicalArm.transform.rotation * Quaternion.AngleAxis(spinValue, Vector3.right),0,-1,QueryTriggerInteraction.Collide);
+        
+        var hitlist = hits.ToList();
+        hitlist.RemoveAll(x=>x.collider.gameObject==physicalArm ||
+                             x.collider.gameObject==grabber.gameObject);
+        //todo revoir le blocker car du coup ça déconne quand on grab l'autre joueur
+        //Debug.Log(hitlist.Count);
+        //if(hitlist.Any(x=>x.collider.gameObject.CompareTag("ArmBlocker")))return;
     }
 
     private void GrabRelease()
@@ -78,36 +85,32 @@ public class Player : Grabbable
         }
     }
 
-    private void ExtendRetract1Axis(Vector3 armIn,out Vector3 tempArm )
+    private void ExtendRetract1Axis(out Vector3 tempArm)
     {
         float extendValue = extendRetractAction.ReadValue<float>() * Time.deltaTime * extentionSpeed; 
-        float armMagnitude = arm.magnitude + extendValue;
+        float armMagnitude = armVector.magnitude + extendValue;
         armMagnitude = Mathf.Clamp(armMagnitude, minExtentionLength, maxExtentionLength);
-        tempArm = armIn;
-        tempArm = tempArm.normalized * armMagnitude;
+        tempArm = transform.forward * armMagnitude;
     }
 
-    private void Spin1Axis(out float spinValue,Vector3 tempArmIn,out Vector3 tempArm)
+    private void Spin1Axis()
     {
-         spinValue = spinAction.ReadValue<float>() * Time.deltaTime * rotationSpeed;
-        tempArm = tempArmIn;
-        tempArm = Quaternion.AngleAxis(spinValue, Vector3.up) * tempArm;
-        
-        
-
+        float spinValue = spinAction.ReadValue<float>() * Time.fixedDeltaTime * rotationSpeed;
+        rb.angularVelocity = new Vector3(0, spinValue, 0);
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        
+        Gizmos.DrawWireSphere(transform.position, maxExtentionLength);
     }
 
-    private void UpadateVisualArm()
+    private void UpdateVisualArm()
     {
-        visualArm.transform.localScale = new Vector3(visualArm.transform.localScale.x,arm.magnitude / 2,visualArm.transform.localScale.z);
-        visualArm.transform.position = transform.position+ arm/2;
-       
+        physicalArm.transform.localScale = new Vector3(physicalArm.transform.localScale.x, 
+                                                    (armVector.magnitude - (Mathf.Abs((armOrigin.position - transform.position).magnitude))) / 2,
+                                                    physicalArm.transform.localScale.z);
+        physicalArm.transform.position = armOrigin.position + (armVector - (armOrigin.position - transform.position))/2;
     }
     
 }
