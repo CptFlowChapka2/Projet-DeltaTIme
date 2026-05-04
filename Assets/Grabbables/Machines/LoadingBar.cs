5933d3c10ec534582a570ebc8fbf8d0d47c6526f
COLIN-MJM <matthieu.j.m.colin@gmail.com>
2026-05-04 14:32:26 +0200

    position réglée sur loadingBar

--- Assets/Grabbables/Machines/LoadingBar.cs
+++ Assets/Grabbables/Machines/LoadingBar.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class LoadingBar : MonoBehaviour
 {
     [SerializeField] private Transform finalTransform;
+    [SerializeField] private Transform startingTransform;
     [SerializeField] private MeshRenderer meshRenderer;
     public float localTickCounter = 0;
     public float numberOfTicksOnRuleToFollow = 0;
@@ -15,6 +16,7 @@ public class LoadingBar : MonoBehaviour
         numberOfTicksOnRuleToFollow = ruleTicks;
         if (numberOfTicksOnRuleToFollow == 0) return;
         transform.localScale = new Vector3(finalTransform.localScale.x / numberOfTicksOnRuleToFollow, 1f, 0.8f);
+        transform.localPosition = new Vector3(startingTransform.localPosition.x, 0.01f, 0f);
     }
     
     public void ExtendLoadingBar()
@@ -26,6 +28,9 @@ public class LoadingBar : MonoBehaviour
             localTickCounter = 1;
         }
         transform.localScale = new Vector3((finalTransform.localScale.x / numberOfTicksOnRuleToFollow) * localTickCounter, 1f, 0.8f);
+        //ligne de l'enfer (set la position relativement à deux points pivots)
+        transform.localPosition = new Vector3(startingTransform.localPosition.x + 
+                                              (localTickCounter * (finalTransform.localPosition.x - startingTransform.localPosition.x) / numberOfTicksOnRuleToFollow), 0.01f, 0f);
     }
 
     public void ChangeColor(Color color)
