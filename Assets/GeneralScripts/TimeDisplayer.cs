b20ffa826d10ab94089cd20d891436b82020ab61
COLIN-MJM <matthieu.j.m.colin@gmail.com>
2026-05-27 14:34:33 +0200

    clock plus jolie

--- Assets/GeneralScripts/TimeDisplayer.cs
+++ Assets/GeneralScripts/TimeDisplayer.cs
@@ -7,24 +7,16 @@ public class TimeDisplayer : MonoBehaviour
 {
     [SerializeField] private LvlInfos lvlInfos;
     [SerializeField] private GameObject needlePivot;
-    [SerializeField] private Image needle;
+    [SerializeField] private CanvasRenderer needle;
     [SerializeField] private TextMeshProUGUI nbOfTicks;
 
     private void Update()
     {
         nbOfTicks.text = (lvlInfos.tNbrTick).ToString();
-        float decreaseValue = 255 - (62 * lvlInfos.timer * lvlInfos.timer);
         needlePivot.transform.Rotate(transform.forward, -180 * Time.deltaTime);
+        
+        float alphaValue = Mathf.Clamp((lvlInfos.timer / 2), 0.1f, 1);
 
-        if (lvlInfos.timer < 0.05f || lvlInfos.timer > 1.95f)
-        {
-            needle.color = Color.blue;
-            needle.rectTransform.localScale = new Vector3(0.15f, 1.5f, 1.5f);
-        }
-        else
-        {
-            needle.color = Color.red;
-            needle.rectTransform.localScale = new Vector3(0.1f, 1f, 1f);
-        }
+        needle.SetAlpha(alphaValue);
     }
 }
