9cf1d5e5e77f007d46c0c8554e6166e311ed1683
COLIN-MJM <matthieu.j.m.colin@gmail.com>
2026-05-29 14:33:10 +0200

    ui degradé

--- Assets/UI/UIManager.cs
+++ Assets/UI/UIManager.cs
@@ -111,31 +111,31 @@ public class UIManager : MonoBehaviour
         hexName.text = hex.hexName;
         hexRule.sprite = hex.hexUIInfo;
 
-        switch (hexName.text)
-        {
-            case "Crevasse":
-                hexUIBox.sprite = hexVariants[0];
-                break;
-            case "Case de Base":
-                hexUIBox.sprite = hexVariants[1];
-                break;
-            case "Forêt":
-                hexUIBox.sprite = hexVariants[2];
-                break;
-            case "Lac Magique":
-                hexUIBox.sprite = hexVariants[3];
-                break;
-            case "Montagne":
-                hexUIBox.sprite = hexVariants[4];
-                break;
-            case "Case entoilée":
-                hexUIBox.sprite = hexVariants[5];
-                break;
-            case "Case Désolée":
-                hexUIBox.sprite = hexVariants[6];
-                break;
-                
-        }
+        // switch (hexName.text)
+        // {
+        //     case "Crevasse":
+        //         hexUIBox.sprite = hexVariants[0];
+        //         break;
+        //     case "Case de Base":
+        //         hexUIBox.sprite = hexVariants[1];
+        //         break;
+        //     case "Forêt":
+        //         hexUIBox.sprite = hexVariants[2];
+        //         break;
+        //     case "Lac Magique":
+        //         hexUIBox.sprite = hexVariants[3];
+        //         break;
+        //     case "Montagne":
+        //         hexUIBox.sprite = hexVariants[4];
+        //         break;
+        //     case "Case entoilée":
+        //         hexUIBox.sprite = hexVariants[5];
+        //         break;
+        //     case "Case Désolée":
+        //         hexUIBox.sprite = hexVariants[6];
+        //         break;
+        //         
+        // }
     }
 
     private void DisplayIngredientsUI()
