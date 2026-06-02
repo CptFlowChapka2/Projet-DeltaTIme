a01e98b158f02841183c357d615ff5a4e8ae0b56
COLIN-MJM <matthieu.j.m.colin@gmail.com>
2026-06-02 13:42:04 +0200

    ++ outlines

--- Assets/Hexes/NormalHex/OutlineColorChanger.cs
+++ Assets/Hexes/NormalHex/OutlineColorChanger.cs
@@ -5,11 +5,8 @@ public class OutlineColorChanger : MonoBehaviour
 {
     [SerializeField] private Hex hex;
     
-    [SerializeField] private Sprite outlinePMouseSprite;
     [SerializeField] private Color outlinePMouseColor;
-    [SerializeField] private Sprite outlinePKeyboardSprite;
     [SerializeField] private Color outlinePKeyboardColor;
-    [SerializeField] private Sprite outlineAllPlayersSprite;
     [SerializeField] private Color outlineAllPlayersColor;
     [SerializeField] private SpriteRenderer outlineSpriteRenderer;
     
@@ -26,22 +23,17 @@ public class OutlineColorChanger : MonoBehaviour
             {
                 if (hex.isPlayerHovering[1])
                 {
-                    currentSprite = outlineAllPlayersSprite;
                     currentColor = outlineAllPlayersColor;
                 }
                 else
                 {
-                    currentSprite = outlinePMouseSprite;
                     currentColor = outlinePMouseColor;
                 }
             }
             else
             {
-                currentSprite = outlinePKeyboardSprite;
                 currentColor = outlinePKeyboardColor;
             }
-            
-            outlineSpriteRenderer.sprite = currentSprite;
             
             float alphaValue = Mathf.Clamp(Mathf.Abs(1 - hex.lvlInfos.timer), 0.6f, 1f);
             currentColor.a = alphaValue;
